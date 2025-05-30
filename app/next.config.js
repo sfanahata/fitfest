608e8b0af1b1e15a394fe4ad3893c10d2db8719d
Shannon Anahata <shannonanahata@gmail.com>
2025-05-30 12:34:51 -0700

    fixing vercel build

--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  webpack: (config) => {
+    // Ignore OpenTelemetry warnings
+    config.ignoreWarnings = [
+      { module: /node_modules\/@opentelemetry/ },
+      { module: /node_modules\/require-in-the-middle/ },
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
