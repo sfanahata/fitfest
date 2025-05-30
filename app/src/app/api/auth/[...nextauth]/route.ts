537187f7e2f78def199fe1c68f3c28f6b9dff4d1
Shannon Anahata <shannonanahata@gmail.com>
2025-05-30 12:38:44 -0700

    still fixing vercel build

--- app/src/app/api/auth/[...nextauth]/route.ts
+++ app/src/app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,4 @@ import { authOptions } from '@/lib/auth.config';
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions }; 
+export { handler as GET, handler as POST }; 
